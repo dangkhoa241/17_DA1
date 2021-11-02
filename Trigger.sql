99531ef59c4a44debc6f3aca655a95ae8d5d1fd2
kieuloansone <phamthikieuloan99@gmail.com>
2021-11-02 11:02:48 +0700

    Query & Trigger

---
+++ Trigger.sql
@@ -0,0 +1,26 @@
+﻿use QLBH
+go
+--Trigger a.Thành tiền CTHD= (Số lượng * (Giá bán-Giá giảm))
+Create trigger trg_CTHD On CT_HoaDon
+For insert, update
+As
+Begin
+update CT_HoaDon set ThanhTien=(Select SoLuong*(GiaBan-GiaGiam) 
+								from inserted )
+End
+
+
+
+GO
+--Trigger b.Tổng tiền (mahd) = sum (ThanhTien) cthd(mahd)
+
+alter trigger t_HD On CT_HoaDon
+For insert, update, delete
+As
+Begin
+update HoaDon set TongTien=(Select Sum(CT_HoaDon.ThanhTien)
+							from  CT_HoaDon
+							where HoaDon.MaHD=CT_HoaDon.MaHD
+							group by CT_HoaDon.MaHD)
+End
+go
---
+++ cau a,b.sql
@@ -0,0 +1,15 @@
+﻿use QLBH
+go
+--Query
+--a. Cho danh sách các hoá đơn lập trong năm 2020
+select *
+from HoaDon
+where year(NgayLap)=2020
+--b. Cho danh sách các khách hàng ở TPHCM
+select *
+from KhachHang
+where Tpho='TPHCM'
+--c. Cho danh sách các sản phẩm có giá trong một khoảng từ....đến
+select *
+from SanPham
+where Gia between 100000 and 200000
